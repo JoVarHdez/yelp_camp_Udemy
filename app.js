1018daacf0540d380b5338c3b86d21343afa2432
JoVarHdez <papinchovargas@gmail.com>
2018-12-12 16:54:02 -0600

    Update app.js

--- app.js
+++ app.js
@@ -64,6 +64,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 // LISTENER
-app.listen(3000, function(){
+app.listen( process.env.PORT || 5000, function(){
   console.log("The YelpCamp Server Has Started!");
 });
