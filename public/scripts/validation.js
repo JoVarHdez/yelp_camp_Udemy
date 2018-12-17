504bbea3c468265980187075c882b674861b44d0
JoVarHdez <papinchovargas@gmail.com>
2018-12-16 18:52:28 -0600

    Try jquery-validation

---
+++ public/scripts/validation.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $("#createUser").validate({
+    rules:{
+      name:{
+        required: true,
+        rangelength: [3,45]
+      },
+    },
+    errorClass: "help-inline",
+    errorElement: "span",
+    highlight:function(element, errorClass, validClass){
+      $(element).parents(".form-group").addClass("error");
+    },
+    unhighligth:function(element, errorClass, validClass){
+      $(element).parents(".form-group").removeClass("error");
+    },
+  })
+});
