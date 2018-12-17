80ef5b6b89fc5fa7866e34f556b79f9ddc7b560c
JoVarHdez <papinchovargas@gmail.com>
2018-12-16 19:10:05 -0600

    Update validation.js

--- public/scripts/validation.js
+++ public/scripts/validation.js
@@ -1,18 +1,32 @@
 $(document).ready(function(){
   $("#createUser").validate({
     rules:{
+      username:{
+        required: true,
+        minlength: 6
+      },
+      email: "required",
       firstName:{
         required: true,
         rangelength: [3,45]
       },
+      lastName:{
+        required: true,
+        rangelength: [3,45]
+      },
+      image: "required",
+      password: {
+        required: true,
+        rangelength: [6,18]
+      }
     },
     errorClass: "help-inline",
     errorElement: "span",
     highlight:function(element, errorClass, validClass){
-      $(element).parents(".form-group").addClass("error");
+      $(element).parents(".form-group").addClass("text-danger");
     },
     unhighligth:function(element, errorClass, validClass){
-      $(element).parents(".form-group").removeClass("error");
+      $(element).parents(".form-group").removeClass("text-danger");
     },
   });
 });
